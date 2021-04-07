85782a84e39e995f6fa66d99c1a320bdc9a48d3a
Dio <gank23456@gmail.com>
2021-04-07 20:01:08 +0800

    2021/4/7 backend v1

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var userSchema = new mongoose.Schema({
 	email:{type:String, unique:true, required: true},
   password: String,
   schoolname:{type:String, required: true},
-  ID:{Number, required: true},
+  ID:{type:Number, required: true},
   username:String,
   avatar:Number,
   allScore:String,
