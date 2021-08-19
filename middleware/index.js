d1cfc07c98927f0fbbb03d7ea2b84b441206b84b
Dio <gank23456@gmail.com>
2021-08-19 16:47:23 +0800

    2021/8/19 passport+jwt

--- middleware/index.js
+++ middleware/index.js
@@ -4,37 +4,45 @@ var user = require("../models/user")
 var middleware = {};
 
 middleware.isLogin = function(req, res, next){
-	const token = req.header('Authorization').replace('Bearer ', '')
-	jwt.verify(token, "ZaWarudo", (err, decode)=>{
-		if(err){
-			res.status(500).json({ message: '驗證失敗，請重新登入'});
-		}
-		else{
-			next()
-		}
-	})
+	if(req.header('Authorization')){
+		const token = req.header('Authorization').replace('Bearer ', '')
+		jwt.verify(token, "ZaWarudo", (err, decode)=>{
+			if(err){
+				res.status(500).json({ message: '驗證失敗，請重新登入'});
+			}
+			else{
+				next()
+			}
+		})
+	}else{
+		res.status(500).json({ message: '驗證失敗，請重新登入'});
+	}
 }
 
 middleware.checkOwnership = function(req, res, next){
-	const token = req.header('Authorization').replace('Bearer ', '')
-	jwt.verify(token, "ZaWarudo", (err, decode)=>{
-		if(err){
-			res.status(500).json({ message: "You Don't Have Permission To Do This."});
-		}
-		else{
-			room.findById(req.params.id, function(err, foundroom){
-				if(err){
-					res.status(500).json({ message: 'Something Get Wrong!'});
-				}else{
-					if(foundroom.email.equals(decode.email)){
-						next();
+	if(req.header('Authorization')){
+		const token = req.header('Authorization').replace('Bearer ', '')
+		jwt.verify(token, "ZaWarudo", (err, decode)=>{
+			if(err){
+				res.status(500).json({ message: "You Don't Have Permission To Do This."});
+			}
+			else{
+				room.findById(req.params.id, function(err, foundroom){
+					if(err){
+						res.status(500).json({ message: 'Something Get Wrong!'});
 					}else{
-						res.status(500).json({ message: "You Don't Have Permission To Do This."});
+						if(foundroom.email.equals(decode.email)){
+							next();
+						}else{
+							res.status(500).json({ message: "You Don't Have Permission To Do This."});
+						}
 					}
-				}
-			});
-		}
-	})
+				});
+			}
+		})
+	}else{
+		res.status(500).json({ message: '驗證失敗，請重新登入'});
+	}
 }
 
 module.exports = middleware;
