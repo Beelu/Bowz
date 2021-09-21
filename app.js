a1aa7226c2769308491003b19d9e8ca95e98081d
james7134 <74014000+james7134@users.noreply.github.com>
2021-09-22 01:05:20 +0800

    mod id transc

--- app.js
+++ app.js
@@ -1133,7 +1133,7 @@ io.on('connection', (socket) => {
 			if(thisRoom){
 				var allUsers = thisRoom.Users;//獲取所有Users
 
-				if(all_Users){
+				if(allUsers){
 					var payer = allUsers.get(data.payer_id);//獲取付款者ID	
 					var receiver = allUsers.get(data.receiver_id);//獲取付款者ID
 
