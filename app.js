9cdb43956a456ff3bed622ab20db760aee773e2f
james7134 <74014000+james7134@users.noreply.github.com>
2021-10-29 15:04:56 +0800

    downloadcsv

--- app.js
+++ app.js
@@ -541,7 +541,7 @@ app.post("/downloadCSV", (req,res) => {
 				if(err){
 					msg = msg+ "error found!";
 				}
-				var record_ = foundrecord.transactions;
+				var record_ = foundrecord;
 				res.json({message:"successfully create TranscReocrdCSV.", record: record_});
 		});
 		
